0f24a77f799228573febe73564fb1aab228ea277
Franziska Dieckmann <franziska.dieckmann@tu-dortmund.de>
2018-06-09 14:52:34 +0200

    first 100 relevance comments

--- quickstart.py
+++ quickstart.py
@@ -19,11 +19,43 @@ SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
 API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
 
+categories = {"2":"Autos & Vehicles",
+"1":"Film & Animation",
+"10":"Music",
+"15":"Pets & Animals",
+"17":"Sports",
+"18":"Short Movies",
+"19":"Travel & Events",
+"20":"Gaming",
+"21":"Videoblogging",
+"22":"People & Blogs",
+"23":"Comedy",
+"24":"Entertainment",
+"25":"News & Politics",
+"26":"Howto & Style",
+"27":"Education",
+"28":"Science & Technology",
+"29":"Nonprofits & Activism",
+"30":"Movies",
+"31":"Anime/Animation",
+"32":"Action/Adventure",
+"33":"Classics",
+"34":"Comedy",
+"35":"Documentary",
+"36":"Drama",
+"37":"Family",
+"38":"Foreign",
+"39":"Horror",
+"40":"Sci-Fi/Fantasy",
+"41":"Thriller",
+"42":"Shorts",
+"43":"Shows",
+"44":"Trailers"}
 
-def get_authenticated_service():
-    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-    credentials = flow.run_console()
-    return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+def get_authenticated_service(): 
+    #flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+    #credentials = flow.run_console()
+    return build(API_SERVICE_NAME, API_VERSION, developerKey="irgendeinKey")
 
 
 def channels_list_by_username(service, **kwargs):
@@ -31,17 +63,41 @@ def channels_list_by_username(service, **kwargs):
         **kwargs
     ).execute()
 
+
     print('This channel\'s ID is %s. Its title is %s, and it has %s views.' %
           (results['items'][0]['id'],
            results['items'][0]['snippet']['title'],
            results['items'][0]['statistics']['viewCount']))
 
 
+def commentsSearch(service, video_id):
+    results = service.commentThreads().list(
+    part="snippet",
+    videoId=video_id,
+    textFormat="plainText",
+    maxResults = 100,
+    order = "relevance"
+    ).execute()
+
+    for item in results["items"]:
+        comment = item["snippet"]["topLevelComment"] 
+        author = comment["snippet"]["authorDisplayName"]
+        text = comment["snippet"]["textDisplay"]
+        print("Comment by %s: %s" % (author, text)) 
+
+
+
+    # ------Kommentare eines Kommentars
+    # parent_id = results["items"][0]["id"]      
+    # comments = service.comments.list( part='snippet', parentId=parent_id).execute()
+    # print(comments)
+
 if __name__ == '__main__':
     # When running locally, disable OAuthlib's HTTPs verification. When
     # running in production *do not* leave this option enabled.
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
     service = get_authenticated_service()
-    channels_list_by_username(service,
-                              part='snippet,contentDetails,statistics',
-                              forUsername='GoogleDevelopers')
+    commentsSearch(service,"naW9U8MiUY0")
+    # channels_list_by_username(service,
+    #                           part='snippet,contentDetails,statistics',
+    #                           forUsername='GoogleDevelopers')
